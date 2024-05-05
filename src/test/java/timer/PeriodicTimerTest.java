b9d36a9f429ce5cba930a8125e77d46289ef84a2
Alexis Thierry <alexist103@gmail.com>
2024-05-05 15:28:48 +0200

    [TEST] Adding PeriodicTimerTest

--- src/test/java/timer/PeriodicTimerTest.java
+++ src/test/java/timer/PeriodicTimerTest.java
@@ -14,6 +14,7 @@ class PeriodicTimerTest {
 
     // ----CONSTRUCTOR----
 
+    // PeriodicTimer(int at)
     @Test // TEST 1
     void testPeriodicTimerConstructorWithPeriod() {
         int period = 1000;
@@ -34,5 +35,68 @@ class PeriodicTimerTest {
         PeriodicTimer periodicTimer = new PeriodicTimer(period);
         assertEquals(period, periodicTimer.getPeriod());
         assertEquals(period, periodicTimer.next());
+    }
+
+    // ----next()----
+
+    @Test // TEST 1
+    void testNextWithMoreOrLessNotNull() throws Exception {
+        int period = 1000;
+        int next = 500;
+        RandomTimer randomTimer = new RandomTimer(RandomTimer.randomDistribution.POISSON, 1500); // Assuming RandomTimer constructor takes min and max as arguments
+        PeriodicTimer timer = new PeriodicTimer(period, next, randomTimer);
+        assertEquals(next, timer.next());
+    }
+
+    @Test // TEST 2
+    void testNextWithValidMoreOrLess() throws Exception {
+        int period = 1000;
+        int next = 500;
+        RandomTimer randomTimer = new RandomTimer(RandomTimer.randomDistribution.POISSON, 1500);
+        PeriodicTimer timer = new PeriodicTimer(period, next, randomTimer);
+        assertNotEquals(500, timer.next());
+    }
+
+    @Test // TEST 3
+    void testNextWithNullMoreOrLess() {
+        int period = 1000;
+        int next = 500;
+        PeriodicTimer timer = new PeriodicTimer(period, next, null);
+        assertEquals(500, timer.next());
+    }
+
+    @Test // TEST 4
+    void testNextWithNullMoreOrLessReturnsNextAndUpdateNextToPeriod() {
+        int period = 1000;
+        int next = 500;
+        PeriodicTimer timer = new PeriodicTimer(period, next, null);
+        assertEquals(500, timer.next());
+    }
+
+    @Test // TEST 5
+    void testNextWithValidMoreOrLessReturnsNextAndUpdateNextToPeriodPlusDifference() throws Exception {
+        int period = 1000;
+        int next = 500;
+        RandomTimer randomTimer = new RandomTimer(RandomTimer.randomDistribution.POISSON, 1500);
+        PeriodicTimer timer = new PeriodicTimer(period, next, randomTimer);
+        assertEquals(500, timer.next());
+    }
+
+    // ----hasNext()----
+    @Test // TEST 1
+    void testHasNextAlwaysReturnsTrue() {
+        int period = 1000;
+        PeriodicTimer timer = new PeriodicTimer(period);
+        assertTrue(timer.hasNext());
+    }
+
+    @Test
+    void testHasNextReturnsTrueAfterMultipleNextCalls() {
+        int period = 1000;
+        PeriodicTimer timer = new PeriodicTimer(period);
+        for (int i = 0; i < 10; i++) {
+            timer.next();
+        }
+        assertTrue(timer.hasNext());
     }
 }
