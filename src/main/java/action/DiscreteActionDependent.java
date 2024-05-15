13a3c0c0c0bba101a557e4d7f3223587ec368806
Alexis Thierry <alexist103@gmail.com>
2024-05-15 15:37:44 +0200

    [REFRACTORING] Adding javadoc

--- src/main/java/action/DiscreteActionDependent.java
+++ src/main/java/action/DiscreteActionDependent.java
@@ -17,22 +17,31 @@ public class DiscreteActionDependent implements DiscreteActionInterface {
 	protected TreeSet<DiscreteAction> depedentActions;
 	private Iterator<DiscreteAction> it;
 	protected DiscreteAction currentAction;
-	
-	
+
+
 	/**
-	 * Construct a series of dependent actions, first action (method) called is baseMethodName, then method nextMethod() is called to select the next action. 
-	 * 
-	 * @param o
-	 * @param baseMethodName
-	 * @param timerBase
-	 */	
+	 * Constructs a DiscreteActionDependent object. This object represents a series of dependent actions.
+	 * The first action (method) called is the baseMethodName, then the method nextMethod() is called to select the next action.
+	 *
+	 * @param o The object on which the base method will be invoked.
+	 * @param baseMethodName The name of the base method to be invoked on the object 'o'.
+	 * @param timerBase The Timer object associated with the base action. This is used to manage the timing of the base action.
+	 */
 	public DiscreteActionDependent(Object o, String baseMethodName, Timer timerBase){
 		this.baseAction = new DiscreteAction(o, baseMethodName, timerBase);
 		this.depedentActions = new TreeSet<DiscreteAction>();
 		this.it = this.depedentActions.iterator();
 		this.currentAction = this.baseAction;
 	}
-	
+
+	/**
+	 * Adds a dependent action to the DiscreteActionDependent object.
+	 * This method creates a new DiscreteAction with the provided parameters and adds it to the set of dependent actions.
+	 *
+	 * @param o The object on which the dependent method will be invoked.
+	 * @param depentMethodName The name of the dependent method to be invoked on the object 'o'.
+	 * @param timerDependence The Timer object associated with the dependent action. This is used to manage the timing of the dependent action.
+	 */
 	public void addDependence(Object o, String depentMethodName, Timer timerDependence) {
 		this.depedentActions.add(new DiscreteAction(o, depentMethodName, timerDependence));
 	}
@@ -85,6 +94,10 @@ public class DiscreteActionDependent implements DiscreteActionInterface {
 		}
 	}
 */
+	/**
+	 * Reinitializes the DiscreteActionDependent object.
+	 * This method iterates over all dependent actions and updates their time lapses.
+	 */
 	private void reInit() {
 		//this.baseAction.updateTimeLaps();
 		for (Iterator<DiscreteAction> iter = this.depedentActions.iterator(); iter.hasNext(); ) {
@@ -92,7 +105,13 @@ public class DiscreteActionDependent implements DiscreteActionInterface {
 		    //element.updateTimeLaps();
 		}		
 	}
-	
+
+	/**
+	 * Determines the next action to be executed in the sequence of dependent actions.
+	 * If the current action is the base action, it sets the iterator to the beginning of the dependent actions and sets the current action to the next action in the set.
+	 * If the current action is the last action in the dependent actions set, it resets the current action back to the base action and reinitializes the DiscreteActionDependent object.
+	 * If the current action is neither the base action nor the last action in the set, it simply moves the current action to the next action in the set.
+	 */
 	public void nextMethod(){
 		if (this.currentAction == this.baseAction){
 			this.it = this.depedentActions.iterator();
@@ -104,7 +123,13 @@ public class DiscreteActionDependent implements DiscreteActionInterface {
 			this.currentAction = this.it.next();
 		}
 	}
-	
+
+	/**
+	 * Reduces the time lapses of all dependent actions by a specified amount.
+	 * This method iterates over all dependent actions and calls their spendTime method with the provided time 't'.
+	 *
+	 * @param t The amount of time to be subtracted from the time lapses of all dependent actions.
+	 */
 	public void spendTime(int t) {
 		for (Iterator<DiscreteAction> iter = this.depedentActions.iterator(); iter.hasNext(); ) {
 		    DiscreteAction element = iter.next();
